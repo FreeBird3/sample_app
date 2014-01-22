8544d9c20da40ec187ba5937e5fed8154d1705f5
Tucker Bird <tucker.bird@gmail.com>
2014-01-22 12:17:25 -0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,10 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
-
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should be_valid }
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -32,6 +34,12 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 
   describe "when email format is invalid" do
     it "should be invalid" do
