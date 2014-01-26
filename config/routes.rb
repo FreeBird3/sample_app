431c95b9ae53c9160168874411de88e28d045bd0
Tucker Bird <tucker.bird@gmail.com>
2014-01-25 16:28:31 -0800

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  root            to: 'static_pages#home'
+  root 'static_pages#home'
   get '/signup',  to: 'users#new'
   get '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
