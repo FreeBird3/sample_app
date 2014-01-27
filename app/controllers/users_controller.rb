c19fa6f3c70e4de7dfb657ac0c65154eb8c80832
Tucker Bird <tucker.bird@gmail.com>
2014-01-26 22:37:48 -0800

    Finished Ch. 9 Excercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user_filter, only: [:new, :create]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -30,9 +31,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
+    user = User.find(params[:id])
+    if (current_user == user) && (current_user.admin?)
+      flash[:error] = "Can't delete own user"
+    else
+      user.destroy
+      flash[:success] = "User deleted."
+    end
+    redirect_to users_path
   end
 
   def update
@@ -59,6 +65,12 @@ end
       unless signed_in?
         store_location
         redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def signed_in_user_filter
+      if signed_in?
+        redirect_to root_path, notice: "Already logged in"
       end
     end
 
